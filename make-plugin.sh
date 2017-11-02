ce3faec0d0b6be2c5781e08b1d14dc44e147d7e6
slawkens <slawkens@gmail.com>
2017-11-02 10:27:01 +0100

    * updated to latest MyAAC (dynamic menus)
    * minimum version to install is now 0.5.1 (cause of new addresses)
    * added make-plugin.zip bash script to create plugin .zip file

---
+++ make-plugin.sh
@@ -0,0 +1,1 @@
+zip -r trees-template.zip templates/ plugins/ -x */\.*
--- templates/trees/template.php
+++ templates/trees/template.php
@@ -1,3 +1,6 @@
+<?php
+defined('MYAAC') or die('Direct access not allowed!');
+?>
 <!DOCTYPE HTML>
 <html>
 <head>
@@ -15,7 +18,6 @@
 	<header>
 	  <div id="logo">
 		<div id="logo_text">
-		<!-- class="logo_colour", allows you to change the colour of the text -->
 		<h1><a href="<?php echo getLink('news'); ?>">My<span class="logo_colour">AAC</span></a></h1>
 		<h2>The best AAC and CMS ever made!</h2>
 		</div>
@@ -23,50 +25,15 @@
 	<nav>
 		<div id="menu_container">
 		  <ul class="sf-menu" id="nav">
-			<li><a href="<?php echo getLink('news'); ?>">Home</a></li>
-			<li><a href="#">Community</a>
-				<ul>
-					<li><a href="<?php echo getLink('characters'); ?>">Characters</a></li>
-					<li><a href="<?php echo getLink('online'); ?>">Online</a></li>
-					<li><a href="<?php echo getLink('highscores'); ?>">Highscores</a></li>
-					<li><a href="<?php echo getLink('lastkills'); ?>">Last Kills</a></li>
-					<li><a href="<?php echo getLink('houses'); ?>">Houses</a></li>
-					<li><a href="<?php echo getLink('guilds'); ?>">Guilds</a></li>
-					<?php if(isset($config['wars'])): ?>
-						<li><a href="<?php echo getLink('wars'); ?>">Wars</a></li>
-					<?php endif;
-					if($config['otserv_version'] == TFS_03): ?>
-						<li><a href="<?php echo getLink('bans'); ?>">Bans</a></li>
-					<?php endif; ?>
-				</ul>
-			</li>
-			<li><a href="#">Library</a>
-				<ul>
-					<li><a href="<?php echo getLink('creatures'); ?>">Monsters</a></li>
-					<li><a href="<?php echo getLink('spells'); ?>">Spells</a></li>
-					<li><a href="<?php echo getLink('commands'); ?>">Commands</a></li>
-					<li><a href="<?php echo getLink('serverInfo'); ?>">Server Info</a></li>
-					<li><a href="<?php echo getLink('movies'); ?>">Movies</a></li>
-					<li><a href="<?php echo getLink('screenshots'); ?>">Screenshots</a></li>
-					<li><a href="<?php echo getLink('experienceTable'); ?>">Experience Table</a></li>
-				</ul>
-			</li>
-			<li><?php echo $template['link_forum']; ?>Forum</a></li>
-			<li><a href="#">Help</a>
-				<ul>
-					<li><a href="<?php echo getLink('team'); ?>">Support</a></li>
-					<li><a href="<?php echo getLink('faq'); ?>">FAQ</a></li>
-				</ul>
-			</li>
-			<?php if($config['gifts_system']): ?>
-			<li><a href="#">Shop</a>
-				<ul>
-					<li><a href="<?php echo getLink('points'); ?>">Buy points</a></li>
-					<li><a href="<?php echo getLink('gifts'); ?>">Gifts</a></li>
-					<li><a href="<?php echo getLink('gifts/history'); ?>">History</a></li>
-				</ul>
-			</li>
-			<?php endif; ?>
+			<?php
+				$menu_table_exist = tableExist(TABLE_PREFIX . 'menu');
+				if($menu_table_exist) {
+					require_once($template_path . '/menu_dynamic.php');
+				}
+				else {
+					require_once($template_path . '/menu.php');
+				}
+			?>
 		  </ul>
 		</div>
 	</nav>
@@ -75,15 +42,15 @@
 		<div id="sidebar_container">
 			<?php
 			if($logged) {
-				include(TEMPLATES . 'trees/widgets/loggedin.php');
+				include($template_path . '/widgets/loggedin.php');
 			} else {
-				include(TEMPLATES . 'trees/widgets/login.php');
+				include($template_path . '/widgets/login.php');
 			}
 			if($logged && admin()) {
-				include(TEMPLATES . 'trees/widgets/admin.php');
+				include($template_path . '/widgets/admin.php');
 			}
 			
-			foreach(glob(TEMPLATES . 'trees/widgets/*.php') as $file) {
+			foreach(glob( $template_path . '/widgets/*.php') as $file) {
 				$filename = pathinfo($file, PATHINFO_FILENAME);
 				if($filename != "login" && $filename != "loggedin" && $filename != "admin") {
 					include($file);
